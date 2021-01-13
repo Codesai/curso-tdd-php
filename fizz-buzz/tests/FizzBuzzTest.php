24a524a395cbd9cb1276cd58d7e8ead42e0099c1
Miguel Ángel Viera <mangelviera93@gmail.com>
2021-01-13 00:07:58 +0000

    Add FizzBuzz kata

---
+++ fizz-buzz/tests/FizzBuzzTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+
+    /** @test */
+    public function should_change_me() {
+        self::fail("change me!");
+    }
+}
