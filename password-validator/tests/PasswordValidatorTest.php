3a2076feaf83bdb1c9d2ccadeaefc396c911b1a4
Miguel Ángel Viera <mangelviera93@gmail.com>
2021-01-13 00:16:48 +0000

    Add Password Validator Kata

---
+++ password-validator/tests/PasswordValidatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class PasswordValidatorTest extends TestCase
+{
+
+    /** @test */
+    public function should_change_me() {
+        $this->fail("change me!");
+    }
+}
