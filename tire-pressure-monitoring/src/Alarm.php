ab5a1e4eabe9aab7995aa3a13d20c896a3f22ded
Manuel Rivero <trikitrok@gmail.com>
2023-11-29 13:06:46 +0100

    Update Alarm.php

--- tire-pressure-monitoring/src/Alarm.php
+++ tire-pressure-monitoring/src/Alarm.php
@@ -5,15 +5,14 @@ namespace Codesai\TDD\TirePressureMonitoring;
 class Alarm
 {
     private const LOW_PRESSURE_THRESHOLD = 17;
-
     private const HIGH_PRESSURE_THRESHOLD = 21;
 
     private $sensor;
-
-    private $isAlarmOn = false;
+    private $isAlarmOn;
 
     public function __construct()
     {
+        $this->isAlarmOn = false;
         $this->sensor = new Sensor();
     }
 
@@ -22,12 +21,15 @@ class Alarm
         $psiPressureValue = $this->sensor->popNextPressurePsiValue();
 
         if ($psiPressureValue < self::LOW_PRESSURE_THRESHOLD || $psiPressureValue > self::HIGH_PRESSURE_THRESHOLD) {
-            $this->isAlarmOn = true;
+            if(!$this->isAlarmOn) {
+               $this->isAlarmOn = true;
+               var_dump("Alarm activated!");
+            }
+        } else {
+            if($this->isAlarmOn) {
+               $this->isAlarmOn = false;
+               var_dump("Alarm deactivated!");
+            }
         }
-    }
-
-    public function isAlarmOn(): bool
-    {
-        return $this->isAlarmOn;
     }
 }
