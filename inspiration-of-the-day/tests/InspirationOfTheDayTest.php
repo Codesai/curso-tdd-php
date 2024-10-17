102fadb7fc0fd8aa59ce1ee7ae81d0db874195f6
freyes <fran@codesai.com>
2024-10-17 11:20:18 +0100

    add inspiration of the day kata

---
+++ inspiration-of-the-day/tests/InspirationOfTheDayTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace tests;
+
+use PHPUnit\Framework\TestCase;
+
+class InspirationOfTheDayTest extends TestCase
+{
+    /** @test */
+    public function should_change_me()
+    {
+        $this->fail("change me!");
+    }
+}
